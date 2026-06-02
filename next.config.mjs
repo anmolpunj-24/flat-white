e5af0d5418dd79cf1eaca058acd92a432e8eb357
anmolpunj-24 <anmolpunj24@gmail.com>
2026-06-02 16:04:43 +0530

    chnges

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  distDir: "build",
+  // distDir: "build",
   reactStrictMode: true,
-  output:"export"
+  output: "export", // makes an out folder which then is used to make a website live
+  trailingSlash: true, // this line too
+  images: {
+    unoptimized: true,  // this is not a good choice for making a website live.. use node.js option on hostinger and eliminate this line
+  },
 };
 
 export default nextConfig;
